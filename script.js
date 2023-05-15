19a7110e5a0670702faa0081eaf9146c2f60cda9
Briton Otieno <ojwangbriton@gmail.com>
2023-05-15 23:03:50 +0300

    Add js file

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var menuBar = document.querySelector(".menu-bar");
+  var closeButton = document.querySelector(".close-button");
+  var navList = document.querySelector(".nav-list");
+  var navItems = document.querySelectorAll(".nav-list li");
+
+  function openMenu() {
+    if (window.innerWidth <= 768) {
+      menuBar.style.display = "none";
+      closeButton.style.display = "block";
+      navList.style.display = "block";
+    }
+  }
+
+  function closeMenu() {
+    if (window.innerWidth <= 768) {
+      menuBar.style.display = "block";
+      closeButton.style.display = "none";
+      navList.style.display = "none";
+    }
+  }
+
+  function handleMenuItemClick() {
+    closeMenu();
+  }
+
+  menuBar.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", closeMenu);
+  navItems.forEach(function (navItem) {
+    navItem.addEventListener("click", handleMenuItemClick);
+  });
+
+  if (window.innerWidth < 768) {
+    closeButton.style.display = "none";
+  }
+
+  if (window.innerWidth > 768) {
+    closeButton.style.display = "none";
+  }
+});
