bdf9fe27bd33de94891c888e5f9a040628190f64
Briton Otieno <ojwangbriton@gmail.com>
2023-05-24 12:22:37 +0300

    Add links to source and live

--- display.js
+++ display.js
@@ -10,8 +10,8 @@ var projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     featuredImage: "images/Snapshoot-Portfolio.svg",
     technologies: ["HTML", "CSS", "Javascript"],
-    liveLink: "https://example.com/live",
-    sourceLink: "https://example.com/source",
+    liveLink: "https://eccb7.github.io/Portfolio2.0/",
+    sourceLink: "https://github.com/Eccb7/Portfolio2.0",
   },
   {
     name: "Multi-Post Stories",
@@ -24,8 +24,8 @@ var projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     featuredImage: "images/multi-post.svg",
     technologies: ["HTML", "CSS", "Javascript"],
-    liveLink: "https://example.com/live",
-    sourceLink: "https://example.com/source",
+    liveLink: "https://eccb7.github.io/Portfolio2.0/",
+    sourceLink: "https://github.com/Eccb7/Portfolio2.0",
   },
   {
     name: "Facebook 360",
@@ -38,8 +38,8 @@ var projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     featuredImage: "images/Snapshoot-Portfolio.svg",
     technologies: ["HTML", "CSS", "Javascript"],
-    liveLink: "https://example.com/live",
-    sourceLink: "https://example.com/source",
+    liveLink: "https://eccb7.github.io/Portfolio2.0/",
+    sourceLink: "https://github.com/Eccb7/Portfolio2.0",
   },
   {
     name: "Uber Navigation",
@@ -52,8 +52,8 @@ var projects = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
     featuredImage: "images/multi-post.svg",
     technologies: ["HTML", "CSS", "Javascript"],
-    liveLink: "https://example.com/live",
-    sourceLink: "https://example.com/source",
+    liveLink: "https://eccb7.github.io/Portfolio2.0/",
+    sourceLink: "https://github.com/Eccb7/Portfolio2.0",
   },
 ];
 
