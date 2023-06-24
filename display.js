4f8dbf7e1fc2745c40d58c67f2ef7da342ee9e18
Briton Otieno <ojwangbriton@gmail.com>
2023-06-25 00:44:07 +0300

    Add project

--- display.js
+++ display.js
@@ -8,7 +8,7 @@ var projects = [
       "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     descriptionlong:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-    featuredImage: "images/Snapshoot-Portfolio.svg",
+    featuredImage: "images/qwerty.png",
     technologies: ["HTML", "CSS", "Javascript"],
     liveLink: "https://eccb7.github.io/Portfolio2.0/",
     sourceLink: "https://github.com/Eccb7/Portfolio2.0",
